643029850d37af4c3b0e6ed5631be67a54caa9a5
이지원 <jiwon@ijiwon-ui-MacBookPro.local>
2022-07-31 13:57:28 +0900

    Initial commit

---
+++ src/main/java/com/nowij/obingo/obingo/ObingoApplication.java
@@ -0,0 +1,13 @@
+package com.nowij.obingo.obingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObingoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObingoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nowij/obingo/obingo/ObingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nowij.obingo.obingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObingoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
